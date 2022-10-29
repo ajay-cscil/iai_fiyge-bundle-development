b43def477ab94db1e50b6bad026c00f50dd9c346
ajay-cscil <tushar@canstartco.com>
2022-10-29 19:48:04 -0400

    gantt formatting

--- client/webroot/js/common.js
+++ client/webroot/js/common.js
@@ -639,7 +639,7 @@ function initChart(container) {
                                             strokeWidth: 5
                                         },
                                         labelStyle: {
-                                          fontName: "Roboto2",
+                                          fontName: "Roboto",
                                           fontSize: 14,
                                           color: '#757575'
                                         }
