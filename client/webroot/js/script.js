08a28770b4e829a36f21c56828608aa64927262c
ajay-cscil <tushar@canstartco.com>
2023-07-14 17:21:04 +0530

    update

--- client/webroot/js/script.js
+++ client/webroot/js/script.js
@@ -233,6 +233,19 @@ function autoResizeIframe(iframe) {
     }
 }
 
+function getPath(obj) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  if(args.length==1){
+    args=args[0].split('.');
+  }
+  for (var i = 0; i < args.length; i++) {
+    if (!obj || !obj.hasOwnProperty(args[i])) {
+      return false;
+    }
+    obj = obj[args[i]];
+  }
+  return obj;
+}
         
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
 pdfjsLib.GlobalWorkerOptions.workerSrc = '/module/pdf/pdf.worker.min.js';
