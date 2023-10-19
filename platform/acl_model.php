206dd39c68694c8b437d7e6e01239031f7c0e3e3
ajay-cscil <tushar@canstartco.com>
2023-10-19 13:08:49 -0400

    up

--- platform/acl_model.php
+++ platform/acl_model.php
@@ -28,16 +28,14 @@ class acl_model extends \kernel\model
             $dataModelClassObject=$dataModelClass::getInstance();
             $relatedToEditACLCache=$dataModelClassObject->getEditACLCache($relatedTo);
             $relatedToDeleteACLCache=$dataModelClassObject->getDeleteACLCache($relatedTo);
-            $canEdit=isset($data['data'][0]['CAN EDIT']) && isset($data['data'][0]['GROUP ID']);
-            $canDelete=isset($data['data'][0]['CAN DELETE']) && isset($data['data'][0]['GROUP ID']);
             foreach($data['data'] as $k=>$v){
                 $data['data'][$k]['CAN EDIT']='No';
                 $data['data'][$k]['CAN DELETE']='No';
-                if($canEdit){
-                    $data['data'][$k]['CAN EDIT']=(in_array("g{$v['GROUP ID']}",$relatedToEditACLCache)?'Yes':'No');
+                if(isset($v['CAN EDIT'])){
+                    $data['data'][$k]['CAN EDIT']=(in_array("g{$v['CAN EDIT']}",$relatedToEditACLCache)?'Yes':'No');
                 }
-                if($canEdit){
-                    $data['data'][$k]['CAN DELETE']=(in_array("g{$v['GROUP ID']}",$relatedToDeleteACLCache)?'Yes':'No');
+                if(isset($v['CAN DELETE'])){
+                    $data['data'][$k]['CAN DELETE']=(in_array("g{$v['CAN DELETE']}",$relatedToDeleteACLCache)?'Yes':'No');
                 }
             }
         }else if($relatedTo && stripos($data['model_class'], '_acl_cache') !== false){
@@ -45,16 +43,14 @@ class acl_model extends \kernel\model
             $dataModelClassObject=$dataModelClass::getInstance();
             $relatedToEditACLCache=$dataModelClassObject->getEditACLCache($relatedTo);
             $relatedToDeleteACLCache=$dataModelClassObject->getDeleteACLCache($relatedTo);
-            $canEdit=isset($data['data'][0]['CAN EDIT']) && isset($data['data'][0]['USER ID']);
-            $canDelete=isset($data['data'][0]['CAN DELETE']) && isset($data['data'][0]['USER ID']);
             foreach($data['data'] as $k=>$v){
                 $data['data'][$k]['CAN EDIT']='No';
                 $data['data'][$k]['CAN DELETE']='No';
-                if($canEdit){
-                    $data['data'][$k]['CAN EDIT']=(in_array("u{$v['USER ID']}",$relatedToEditACLCache)?'Yes':'No');
+                if(isset($v['CAN EDIT'])){
+                    $data['data'][$k]['CAN EDIT']=(in_array("u{$v['CAN EDIT']}",$relatedToEditACLCache)?'Yes':'No');
                 }
-                if($canEdit){
-                    $data['data'][$k]['CAN DELETE']=(in_array("u{$v['USER ID']}",$relatedToDeleteACLCache)?'Yes':'No');
+                if(isset($v['CAN DELETE'])){
+                    $data['data'][$k]['CAN DELETE']=(in_array("u{$v['CAN DELETE']}",$relatedToDeleteACLCache)?'Yes':'No');
                 }
             }
         }  
