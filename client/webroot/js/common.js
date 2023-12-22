82cbb2b5a5aaba0b40a6d4c40dffd9d812c82534
ajay-cscil <tushar@canstartco.com>
2023-12-22 00:44:18 -0500

    up

--- client/webroot/js/common.js
+++ client/webroot/js/common.js
@@ -352,10 +352,13 @@ function getMapMarkup(uuid, options, map, data, primaryKeys, infowindow, zoomToF
 }
 function getColumnTotalData(listview){
     var columnTotalData=listview.attr('column_total_data');
+    console.log(1,columnTotalData);
     if(columnTotalData){
             columnTotalData=atob(columnTotalData);
+            console.log(2,columnTotalData);
             if(columnTotalData){
                 columnTotalData=JSON.parse(columnTotalData);
+                console.log(3,columnTotalData);
             }
     }
     if(!jQuery.isPlainObject(columnTotalData)){
