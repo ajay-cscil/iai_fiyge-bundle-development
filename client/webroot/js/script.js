a783a99ba90e8edd11c0d5a0d4d83fc1db07af9e
ajay-cscil <tushar@canstartco.com>
2024-02-15 15:21:20 -0500

    up

--- client/webroot/js/script.js
+++ client/webroot/js/script.js
@@ -499,7 +499,21 @@ jQuery('document').ready(function($) {
      * Initialize default ajax loader.
      * @author Tushar Takkar<ttakkar@primarymodules.com>
      */
-    $("body").append('<div id="ajax-loader" class="ui-state-highlight" style="display:none;z-index:100002"><img style="width:300px;" src="/img/page-loading.gif"></div>');
+    $("body").append('<div id="ajax-loader" class="ui-state-highlight" style="display:none;z-index:100002">'+
+        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin:auto;background:#fff;display:block;" width="250px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">'+
+            '<defs>'+
+              '<clipPath id="progress-7x4qtbj3m1o-cp" x="0" y="0" width="100" height="100">'+
+                '<rect x="0" y="0" width="0" height="100">'+
+                  '<animate attributeName="width" repeatCount="indefinite" dur="1s" values="0;100;100" keyTimes="0;0.5;1"></animate>'+
+                  '<animate attributeName="x" repeatCount="indefinite" dur="1s" values="0;0;100" keyTimes="0;0.5;1"></animate>'+
+                '</rect>'+
+              '</clipPath>'+
+            '</defs>'+
+            '<text x="15" y="20" style="font: italic 20px serif;fill: #E15A63;">Loading...</text>'+
+            '<path fill="none" stroke="#e15b64" stroke-width="2.79" d="M18 36.895L81.99999999999999 36.895A13.104999999999999 13.104999999999999 0 0 1 95.10499999999999 50L95.10499999999999 50A13.104999999999999 13.104999999999999 0 0 1 81.99999999999999 63.105L18 63.105A13.104999999999999 13.104999999999999 0 0 1 4.895000000000003 50L4.895000000000003 50A13.104999999999999 13.104999999999999 0 0 1 18 36.895 Z"></path>'+
+            '<path fill="#f8b26a" clip-path="url(#progress-7x4qtbj3m1o-cp)" d="M18 40.99L82 40.99A9.009999999999998 9.009999999999998 0 0 1 91.00999999999999 50L91.00999999999999 50A9.009999999999998 9.009999999999998 0 0 1 82 59.01L18 59.01A9.009999999999998 9.009999999999998 0 0 1 8.990000000000004 50L8.990000000000004 50A9.009999999999998 9.009999999999998 0 0 1 18 40.99 Z"></path>'+
+        '</svg>'+
+    '</div>');
     var bodyWidth = window.innerWidth;
     var bodyHeight = window.innerHeight;
     var ajaxLoaderWidth = $("#ajax-loader").width();
